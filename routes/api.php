9b8c1605d2915179abfe2091e8edeb300d2ce321
Jerry <azhagirishnakar5@gmail.com>
2025-11-17 12:16:32 +0530

    #43 all changes done

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,15 @@
 
 
 use App\Http\Controllers\AdminUserController;
+use App\Http\Controllers\Api\MobileSqlBridgeController;
+use App\Http\Controllers\Api\OrderSupportController;
 use App\Http\Controllers\Api\productcontroller;
 use App\Http\Controllers\Api\FirestoreBridgeController;
 use App\Http\Controllers\Api\SettingsApiController;
 use App\Http\Controllers\Api\VendorController;
 use App\Http\Controllers\Api\Vendor_Reviews;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\SettingsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PaymentController;
@@ -230,17 +234,12 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/mart-items/getSimilarProducts', [MartItemController::class, 'getSimilarProducts']);
     Route::get('/mart-items/getItemsBySectionName', [MartItemController::class, 'getItemsBySectionName']);
     Route::get('/mart-items/getMartVendors', [MartItemController::class, 'getMartVendors']);
-
-
-
-
 });
 
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/vendor/attributes', [SettingsApiController::class, 'getVendorAttributes']);
 });
-
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/vendor/{vendorId}/reviews', [Vendor_Reviews::class, 'getVendorReviews']);
@@ -278,4 +277,13 @@ Route::middleware('auth:sanctum')->group(function () {
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/products', [ProductController::class, 'getAllPublishedProducts']);
+});
+
+Route::get('/settings/ringtone', [SettingsController::class, 'getRingtone']);
+Route::get('/orders/latest-id', [OrderController::class, 'getLatestOrderId']);
+Route::get('/orders/get/{id}', [OrderController::class, 'getOrder']);
+
+Route::post('/mobile/orders/place-basic', [OrderSupportController::class, 'placeOrder']);
+Route::middleware('auth:sanctum')->group(function () {
+Route::post('/mobile/orders', [MobileSqlBridgeController::class, 'createOrder']);
 });
